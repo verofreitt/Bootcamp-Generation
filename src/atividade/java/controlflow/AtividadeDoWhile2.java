0e2a0c4faaf06caec0ad8ed360cad895d9e5a0d5
Verônica Freitas Santos <verofreit@gmail.com>
2023-12-10 23:24:32 -0300

    atividades do while resolvidas

--- src/atividade/java/controlflow/AtividadeDoWhile2.java
+++ src/atividade/java/controlflow/AtividadeDoWhile2.java
@@ -1,10 +1,28 @@
 package atividade.java.controlflow;
+import java.util.Scanner;
 
 public class AtividadeDoWhile2 {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int soma = 0, numero;
+		float contador = 0f;
 		
-
+		do {
+			System.out.println("Digite um número: ");
+			numero = scanner.nextInt();
+			if(numero == 0) {
+				break;
+			}
+			if(numero % 3 == 0) {
+				soma += numero;
+				contador++;
+			}
+			
+		}while(numero != 0);
+		
+		double media = (soma / contador);
+		System.out.println("A média de todos os números múltiplos de 3 é: "+media);
 	}
 
 }
