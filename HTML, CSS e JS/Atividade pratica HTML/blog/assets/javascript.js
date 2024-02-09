f9e13f0195adeab9a75f40b56512bea621536d11
verofreitt <verofreit@gmail.com>
2024-02-09 14:30:48 -0300

    search de personagens com js implementado

---
+++ HTML, CSS e JS/Atividade pratica HTML/blog/assets/javascript.js
@@ -0,0 +1,16 @@
+function searchCharacters() {
+    var input, filter, personagens, personagem, nome, i;
+    input = document.getElementById('searchInput');
+    filter = input.value.toUpperCase();
+    personagens = document.querySelectorAll('.personagem');
+
+    for (i = 0; i < personagens.length; i++) {
+      personagem = personagens[i];
+      nome = personagem.querySelector('.nome-personagem');
+      if (nome.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        personagem.style.display = '';
+      } else {
+        personagem.style.display = 'none';
+      }
+    }
+  }
