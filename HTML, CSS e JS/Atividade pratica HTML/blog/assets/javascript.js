f4f05d9a0875d3978d4944b25e92123e6bc128cb
verofreitt <verofreit@gmail.com>
2024-02-16 12:54:40 -0300

    atividades css

--- HTML, CSS e JS/Atividade pratica HTML/blog/assets/javascript.js
+++ HTML, CSS e JS/Atividade pratica HTML/blog/assets/javascript.js
@@ -1,16 +1,20 @@
 function searchCharacters() {
-    var input, filter, personagens, personagem, nome, i;
-    input = document.getElementById('searchInput');
-    filter = input.value.toUpperCase();
-    personagens = document.querySelectorAll('.personagem');
+  var input, filter, personagens, personagem, nome, i;
+  input = document.getElementById('searchInput');
+  filter = input.value.toUpperCase();
+  personagens = document.querySelectorAll('.personagem');
 
-    for (i = 0; i < personagens.length; i++) {
-      personagem = personagens[i];
-      nome = personagem.querySelector('.nome-personagem');
-      if (nome.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        personagem.style.display = '';
-      } else {
-        personagem.style.display = 'none';
-      }
+  for (i = 0; i < personagens.length; i++) {
+    personagem = personagens[i];
+    nome = personagem.querySelector('.nome-personagem');
+    if (nome.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      personagem.style.display = '';
+    } else {
+      personagem.style.display = 'none';
     }
   }
+}
+  
+  
+  
+  
